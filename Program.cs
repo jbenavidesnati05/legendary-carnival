3353119b47b4286f211be6ab9c6a82ef8cc11ea6
jbenavides <jbenavides@sistecredito.com>
2023-07-09 19:29:29 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Humanizer;
+
+Console.WriteLine("ingrese un nombre");
+var nombre = Console.ReadLine();
+
+Console.WriteLine("ingrese un cargo");
+var cargo = Console.ReadLine();
+
+Console.WriteLine("ingrese su edad");
+var edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad.ToWords(new System.Globalization.CultureInfo("es"))} años ");
